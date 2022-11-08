1d14213b40e482e37104d2231bf745afc58d33e3
tahminarim <tahminarim9@gmail.com>
2022-11-08 20:55:53 +0100

    basic node mongo setup

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 //middle ware
 app.use(cors());
 app.use(express.json());
+
+// mongodb
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7rxnq3x.mongodb.net/?retryWrites=true&w=majority`;
+//console.log(uri)
 
 
 app.get('/',(req,res)=>{
