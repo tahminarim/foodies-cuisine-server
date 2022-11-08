405b198b5d0831026c8e3f4a1241b7f9d6cd8955
tahminarim <tahminarim9@gmail.com>
2022-11-09 00:15:59 +0100

    database created for all menu(recipies)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,10 +10,31 @@ app.use(cors());
 app.use(express.json());
 
 // mongodb
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7rxnq3x.mongodb.net/?retryWrites=true&w=majority`;
 //console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+//crud operations
+async function run(){
+    try{
+        const menuCollection = client.db('foodiesCuisine').collection('allMenu');
+        
+//all menus api for read all menus 
+        app.get('/allmenu', async (req, res) => {
+            const query = {}
+            const cursor = menuCollection.find(query);
+            const menus = await cursor.toArray();
+            res.send(menus);
+        });
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(err=>console.log(err));
 
 
 app.get('/',(req,res)=>{
