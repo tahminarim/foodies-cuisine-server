a66962589e844cf95e66ea9792b2e23a419db20d
tahminarim <tahminarim9@gmail.com>
2022-11-08 20:40:27 +0100

    simple index file running

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middle ware
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/',(req,res)=>{
+    res.send('Foodies Cuisine server is running')
+})
+
+app.listen(port,()=> {
+    console.log(`Foodies Cuisine server running on ${port}`)
+})
